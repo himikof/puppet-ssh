1bad490ec4cf21ac9e16019b3dfd197741a91027
Nikita Ofitserov <Nikita.Ofitserov@transas.com>
2012-08-14 20:50:00 +0400

    Add missing deps

--- manifests/hostkeys.pp
+++ manifests/hostkeys.pp
@@ -6,12 +6,14 @@ class ssh::hostkeys ($exchange_keys) {
             type         => 'dsa',
             key          => $::sshdsakey,
             tag          => 'exchanged',
+            require      => Class['ssh::server::service'],
         }
         @@sshkey { "${fqdn}_rsa":
             host_aliases => [ $::fqdn, $::hostname, $::ipaddress ],
             type         => 'rsa',
             key          => $::sshrsakey,
             tag          => 'exchanged',
+            require      => Class['ssh::server::service'],
         }
     }
 }
