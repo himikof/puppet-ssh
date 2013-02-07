98a8adc62990aed5a6ac51cb76a4c16049b1af8a
Nikita Ofitserov <Nikita.Ofitserov@transas.com>
2013-02-07 17:43:26 +0400

    Export ECDSA SSH host keys too

--- manifests/hostkeys.pp
+++ manifests/hostkeys.pp
@@ -1,19 +1,31 @@
 class ssh::hostkeys ($exchange_keys) {
-
-    if $exchange_keys {
-        @@sshkey { "${::fqdn}_dsa":
-            host_aliases => [ $::fqdn, $::hostname, $::ipaddress ],
-            type         => 'dsa',
-            key          => $::sshdsakey,
-            tag          => 'exchanged',
-            require      => Class['ssh::server::service'],
-        }
-        @@sshkey { "${fqdn}_rsa":
-            host_aliases => [ $::fqdn, $::hostname, $::ipaddress ],
-            type         => 'rsa',
-            key          => $::sshrsakey,
-            tag          => 'exchanged',
-            require      => Class['ssh::server::service'],
-        }
+  if $exchange_keys {
+    if $::sshdsakey {
+      @@sshkey { "${::fqdn}_dsa":
+          host_aliases => [ $::fqdn, $::hostname, $::ipaddress ],
+          type         => 'dsa',
+          key          => $::sshdsakey,
+          tag          => 'exchanged',
+          require      => Class['ssh::server::service'],
+      }
     }
+    if $::sshrsakey {
+      @@sshkey { "${::fqdn}_rsa":
+          host_aliases => [ $::fqdn, $::hostname, $::ipaddress ],
+          type         => 'rsa',
+          key          => $::sshrsakey,
+          tag          => 'exchanged',
+          require      => Class['ssh::server::service'],
+      }
+    }
+    if $::sshecdsakey {
+      @@sshkey { "${::fqdn}_ecdsa":
+          host_aliases => [ $::fqdn, $::hostname, $::ipaddress ],
+          type         => 'ecdsa-sha2-nistp256',
+          key          => $::sshecdsakey,
+          tag          => 'exchanged',
+          require      => Class['ssh::server::service'],
+      }
+    }
+  }
 }
