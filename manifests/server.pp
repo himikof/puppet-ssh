dfb0d0e4bc262ff6d367b9d29a556943f0a2555f
Nikita Ofitserov <Nikita.Ofitserov@transas.com>
2013-02-07 17:22:51 +0400

    Make sshd PasswordAuthentication configurable

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,7 +1,11 @@
 class ssh::server (
   $exchange_keys = false,
+  $password_auth = false,
 ) {
-  include ssh::params, ssh::server::install, ssh::server::config, ssh::server::service
+  include ssh::params, ssh::server::install, ssh::server::service
+  class { 'ssh::server::config':
+    password_auth => $password_auth
+  }
   class { 'ssh::hostkeys':
     exchange_keys => $exchange_keys
   }
--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -1,10 +1,12 @@
-class ssh::server::config {
+class ssh::server::config (
+  $password_auth,
+) {
     file { $ssh::params::sshd_config:
         ensure  => present,
         owner   => "root",
         group   => "root",
         mode    => 0600,
-        source  => "puppet:///modules/${module_name}/sshd_config",
+        content => template("${module_name}/sshd_config.erb"),
         require => Class["ssh::server::install"],
         notify  => Class["ssh::server::service"],
     }
